eae9ac751cfe93d53696fd4044785b55af001ab7
Pavel <Pavel.Vlasov@nasdanika.org>
2024-06-22 22:17:45 -0400

    Cleanup

--- processors/targets/java/src/main/java/module-info.java
+++ processors/targets/java/src/main/java/module-info.java
@@ -1,9 +1,4 @@
 import org.nasdanika.capability.CapabilityFactory;
-import org.nasdanika.models.functionflow.processors.targets.java.EndNodeProcessorFactory;
-import org.nasdanika.models.functionflow.processors.targets.java.FunctionFlowNodeProcessorFactory;
-import org.nasdanika.models.functionflow.processors.targets.java.JavaReflectiveBiFunctionProcessorServiceFactory;
-import org.nasdanika.models.functionflow.processors.targets.java.StartNodeProcessorFactory;
-import org.nasdanika.models.functionflow.processors.targets.java.TransitionConnectionProcessorFactory;
 import org.nasdanika.models.functionflow.processors.targets.java.sync.SyncCapabilityFactory;
 
 module org.nasdanika.models.functionflow.processors.targets.java {
@@ -12,8 +7,7 @@ module org.nasdanika.models.functionflow.processors.targets.java {
 	requires transitive org.nasdanika.models.ecore.graph;
 	requires transitive org.nasdanika.models.graph.processors;
 	
-	exports org.nasdanika.models.functionflow.processors.targets.java;
-	opens org.nasdanika.models.functionflow.processors.targets.java; // For loading resources
+	exports org.nasdanika.models.functionflow.processors.targets.java.sync;
 	opens org.nasdanika.models.functionflow.processors.targets.java.sync to org.nasdanika.common; // For loading resources
 
 	provides CapabilityFactory with 
