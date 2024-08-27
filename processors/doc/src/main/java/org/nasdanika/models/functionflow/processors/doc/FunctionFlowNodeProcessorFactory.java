02f0802f202b3b8fc9a7c47cd175dcef960a6601
Pavel <Pavel.Vlasov@nasdanika.org>
2024-08-27 08:17:00 -0400

    2024.8.0

--- processors/doc/src/main/java/org/nasdanika/models/functionflow/processors/doc/FunctionFlowNodeProcessorFactory.java
+++ processors/doc/src/main/java/org/nasdanika/models/functionflow/processors/doc/FunctionFlowNodeProcessorFactory.java
@@ -5,13 +5,8 @@ import java.util.function.BiFunction;
 import org.eclipse.emf.common.util.URI;
 import org.nasdanika.common.Context;
 import org.nasdanika.common.ProgressMonitor;
-import org.nasdanika.graph.emf.EObjectNode;
-import org.nasdanika.graph.processor.NodeProcessorConfig;
-import org.nasdanika.html.model.app.Action;
-import org.nasdanika.html.model.app.AppFactory;
-import org.nasdanika.html.model.app.graph.WidgetFactory;
+import org.nasdanika.html.model.app.Label;
 import org.nasdanika.models.architecture.processors.doc.ArchitectureNodeProcessorFactory;
-import org.nasdanika.ncore.util.NcoreUtil;
 
 /**
  * Node processor factory to use with {@link EObjectReflectiveProcessorFactory} to generate documentation.
@@ -20,7 +15,7 @@ import org.nasdanika.ncore.util.NcoreUtil;
  */
 public class FunctionFlowNodeProcessorFactory extends ArchitectureNodeProcessorFactory {
 			
-	public FunctionFlowNodeProcessorFactory(Context context, BiFunction<URI, ProgressMonitor, Action> prototypeProvider) {
+	public FunctionFlowNodeProcessorFactory(Context context, BiFunction<URI, ProgressMonitor, Label> prototypeProvider) {
 		super(context, prototypeProvider);
 		// TODO Auto-generated constructor stub
 	}
