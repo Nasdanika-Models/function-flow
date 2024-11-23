d02bfc9d8618b9f1e7d2e7d81856a60dc5032b2d
Pavel Vlasov <Pavel.Vlasov@nasdanika.org>
2024-11-23 11:19:42 -0500

    Stub processor factory methods

--- processors/runtime/src/main/java/module-info.java
+++ processors/runtime/src/main/java/module-info.java
@@ -6,6 +6,7 @@ module org.nasdanika.models.functionflow.processors.runtime {
 	requires transitive org.nasdanika.models.functionflow;
 	requires transitive org.nasdanika.models.ecore.graph;
 	requires transitive org.nasdanika.models.graph.processors;
+	requires org.nasdanika.graph;
 	
 	exports org.nasdanika.models.functionflow.processors.runtime;
 	opens org.nasdanika.models.functionflow.processors.runtime to org.nasdanika.common; // For loading resources
--- processors/runtime/src/main/java/org/nasdanika/models/functionflow/processors/runtime/ProcessorFactory.java
+++ processors/runtime/src/main/java/org/nasdanika/models/functionflow/processors/runtime/ProcessorFactory.java
@@ -9,6 +9,7 @@ import org.nasdanika.common.ProgressMonitor;
 import org.nasdanika.graph.Element;
 import org.nasdanika.graph.Node;
 import org.nasdanika.graph.model.adapters.NodeAdapter;
+import org.nasdanika.graph.processor.ConnectionProcessorConfig;
 import org.nasdanika.graph.processor.NodeProcessorConfig;
 import org.nasdanika.graph.processor.Processor;
 import org.nasdanika.graph.processor.ProcessorInfo;
@@ -29,6 +30,7 @@ public class ProcessorFactory {
 	
 	@Processor(
 			type = NodeAdapter.class,
+			priority = 2,
 			value = "get() instanceof T(org.nasdanika.models.functionflow.FunctionFlow)")
 	public Object createFunctionFlowProcessor(
 		NodeProcessorConfig<?,?> config, 
@@ -66,6 +68,271 @@ public class ProcessorFactory {
 		
 		NodeAdapter element = (NodeAdapter) config.getElement();
 		return new EndProcessor(endResolver == null ? null : endResolver.apply((End) element.get()));
+	}
+	
+	
+	// -----
+	
+	
+	@Processor(
+			type = NodeAdapter.class,
+			priority = 1,
+			value = "get() instanceof T(org.nasdanika.models.functionflow.Call)")
+	public Object createCallProcessor(
+			ConnectionProcessorConfig<?,?> config, 
+			boolean parallel, 
+			BiConsumer<Element,BiConsumer<ProcessorInfo<Object>,ProgressMonitor>> infoProvider,
+			Function<ProgressMonitor, Object> next,		
+			ProgressMonitor progressMonitor) {
+		
+		throw new UnsupportedOperationException();
+	}
+		
+	@Processor(
+			type = NodeAdapter.class,
+			value = "get() instanceof T(org.nasdanika.models.functionflow.Consumer)")
+	public Object createConsumerProcessor(
+			NodeProcessorConfig<?,?> config, 
+			boolean parallel, 
+			BiConsumer<Element,BiConsumer<ProcessorInfo<Object>,ProgressMonitor>> infoProvider,
+			Function<ProgressMonitor, Object> next,		
+			ProgressMonitor progressMonitor) {
+		
+		throw new UnsupportedOperationException();
+	}
+		
+	@Processor(
+			type = NodeAdapter.class,
+			priority = 1,
+			value = "get() instanceof T(org.nasdanika.models.functionflow.ConsumerFlow)")
+	public Object createConsumerFlowProcessor(
+			NodeProcessorConfig<?,?> config, 
+			boolean parallel, 
+			BiConsumer<Element,BiConsumer<ProcessorInfo<Object>,ProgressMonitor>> infoProvider,
+			Function<ProgressMonitor, Object> next,		
+			ProgressMonitor progressMonitor) {
+		
+		throw new UnsupportedOperationException();
+	}
+		
+	@Processor(
+			type = NodeAdapter.class,
+			value = "get() instanceof T(org.nasdanika.models.functionflow.ErrorHandler)")
+	public Object createErrorHandlerProcessor(
+			NodeProcessorConfig<?,?> config, 
+			boolean parallel, 
+			BiConsumer<Element,BiConsumer<ProcessorInfo<Object>,ProgressMonitor>> infoProvider,
+			Function<ProgressMonitor, Object> next,		
+			ProgressMonitor progressMonitor) {
+		
+		throw new UnsupportedOperationException();
+	}
+		
+	@Processor(
+			type = NodeAdapter.class,
+			priority = 1, 
+			value = "get() instanceof T(org.nasdanika.models.functionflow.ErrorTransition)")
+	public Object createErrorTransitionProcessor(
+			ConnectionProcessorConfig<?,?> config, 
+			boolean parallel, 
+			BiConsumer<Element,BiConsumer<ProcessorInfo<Object>,ProgressMonitor>> infoProvider,
+			Function<ProgressMonitor, Object> next,		
+			ProgressMonitor progressMonitor) {
+		
+		throw new UnsupportedOperationException();
+	}
+		
+	@Processor(
+			type = NodeAdapter.class,
+			value = "get() instanceof T(org.nasdanika.models.functionflow.Flow)")
+	public Object createFlowProcessor(
+			NodeProcessorConfig<?,?> config, 
+			boolean parallel, 
+			BiConsumer<Element,BiConsumer<ProcessorInfo<Object>,ProgressMonitor>> infoProvider,
+			Function<ProgressMonitor, Object> next,		
+			ProgressMonitor progressMonitor) {
+		
+		throw new UnsupportedOperationException();
+	}
+		
+	@Processor(
+			type = NodeAdapter.class,
+			priority = 1, 
+			value = "get() instanceof T(org.nasdanika.models.functionflow.Function)")
+	public Object createFunctionProcessor(
+			NodeProcessorConfig<?,?> config, 
+			boolean parallel, 
+			BiConsumer<Element,BiConsumer<ProcessorInfo<Object>,ProgressMonitor>> infoProvider,
+			Function<ProgressMonitor, Object> next,		
+			ProgressMonitor progressMonitor) {
+		
+		throw new UnsupportedOperationException();
+	}
+				
+	@Processor(
+			type = NodeAdapter.class,
+			priority = 1, 			
+			value = "get() instanceof T(org.nasdanika.models.functionflow.Publisher)")
+	public Object createPublisherProcessor(
+			NodeProcessorConfig<?,?> config, 
+			boolean parallel, 
+			BiConsumer<Element,BiConsumer<ProcessorInfo<Object>,ProgressMonitor>> infoProvider,
+			Function<ProgressMonitor, Object> next,		
+			ProgressMonitor progressMonitor) {
+		
+		throw new UnsupportedOperationException();
+	}
+		
+	@Processor(
+			type = NodeAdapter.class,
+			priority = 2, 
+			value = "get() instanceof T(org.nasdanika.models.functionflow.SourceCall)")
+	public Object createSourceCallProcessor(
+			ConnectionProcessorConfig<?,?> config, 
+			boolean parallel, 
+			BiConsumer<Element,BiConsumer<ProcessorInfo<Object>,ProgressMonitor>> infoProvider,
+			Function<ProgressMonitor, Object> next,		
+			ProgressMonitor progressMonitor) {
+		
+		throw new UnsupportedOperationException();
+	}
+		
+	@Processor(
+			type = NodeAdapter.class,
+			priority = 1, 
+			value = "get() instanceof T(org.nasdanika.models.functionflow.SourceConsumer)")
+	public Object createSourceConsumerProcessor(
+			NodeProcessorConfig<?,?> config, 
+			boolean parallel, 
+			BiConsumer<Element,BiConsumer<ProcessorInfo<Object>,ProgressMonitor>> infoProvider,
+			Function<ProgressMonitor, Object> next,		
+			ProgressMonitor progressMonitor) {
+		
+		throw new UnsupportedOperationException();
+	}
+		
+	@Processor(
+			type = NodeAdapter.class,
+			priority = 1, 
+			value = "get() instanceof T(org.nasdanika.models.functionflow.SourceErrorHandler)")
+	public Object createSourceErrorHandlerProcessor(
+			NodeProcessorConfig<?,?> config, 
+			boolean parallel, 
+			BiConsumer<Element,BiConsumer<ProcessorInfo<Object>,ProgressMonitor>> infoProvider,
+			Function<ProgressMonitor, Object> next,		
+			ProgressMonitor progressMonitor) {
+		
+		throw new UnsupportedOperationException();
+	}
+		
+	@Processor(
+			type = NodeAdapter.class,
+			priority = 2, 
+			value = "get() instanceof T(org.nasdanika.models.functionflow.SourceErrorTransition)")
+	public Object createSourceErrorTransitionProcessor(
+			NodeProcessorConfig<?,?> config, 
+			boolean parallel, 
+			BiConsumer<Element,BiConsumer<ProcessorInfo<Object>,ProgressMonitor>> infoProvider,
+			Function<ProgressMonitor, Object> next,		
+			ProgressMonitor progressMonitor) {
+		
+		throw new UnsupportedOperationException();
+	}
+		
+	@Processor(
+			type = NodeAdapter.class,
+			priority = 2, 
+			value = "get() instanceof T(org.nasdanika.models.functionflow.SourceFunction)")
+	public Object createSourceFunctionProcessor(
+			NodeProcessorConfig<?,?> config, 
+			boolean parallel, 
+			BiConsumer<Element,BiConsumer<ProcessorInfo<Object>,ProgressMonitor>> infoProvider,
+			Function<ProgressMonitor, Object> next,		
+			ProgressMonitor progressMonitor) {
+		
+		throw new UnsupportedOperationException();
+	}
+		
+	@Processor(
+			type = NodeAdapter.class,
+			priority = 1, 
+			value = "get() instanceof T(org.nasdanika.models.functionflow.SourceSupplier)")
+	public Object createSourceSupplierProcessor(
+			NodeProcessorConfig<?,?> config, 
+			boolean parallel, 
+			BiConsumer<Element,BiConsumer<ProcessorInfo<Object>,ProgressMonitor>> infoProvider,
+			Function<ProgressMonitor, Object> next,		
+			ProgressMonitor progressMonitor) {
+		
+		throw new UnsupportedOperationException();
+	}
+		
+	@Processor(
+			type = NodeAdapter.class,
+			priority = 1, 
+			value = "get() instanceof T(org.nasdanika.models.functionflow.SourceTransition)")
+	public Object createSourceTransitionProcessor(
+			ConnectionProcessorConfig<?,?> config, 
+			boolean parallel, 
+			BiConsumer<Element,BiConsumer<ProcessorInfo<Object>,ProgressMonitor>> infoProvider,
+			Function<ProgressMonitor, Object> next,		
+			ProgressMonitor progressMonitor) {
+		
+		throw new UnsupportedOperationException();
+	}
+		
+	@Processor(
+			type = NodeAdapter.class,
+			priority = 1, 
+			value = "get() instanceof T(org.nasdanika.models.functionflow.Subscriber)")
+	public Object createSubscriberProcessor(
+			NodeProcessorConfig<?,?> config, 
+			boolean parallel, 
+			BiConsumer<Element,BiConsumer<ProcessorInfo<Object>,ProgressMonitor>> infoProvider,
+			Function<ProgressMonitor, Object> next,		
+			ProgressMonitor progressMonitor) {
+		
+		throw new UnsupportedOperationException();
+	}
+		
+	@Processor(
+			type = NodeAdapter.class,
+			value = "get() instanceof T(org.nasdanika.models.functionflow.Supplier)")
+	public Object createSupplierProcessor(
+			NodeProcessorConfig<?,?> config, 
+			boolean parallel, 
+			BiConsumer<Element,BiConsumer<ProcessorInfo<Object>,ProgressMonitor>> infoProvider,
+			Function<ProgressMonitor, Object> next,		
+			ProgressMonitor progressMonitor) {
+		
+		throw new UnsupportedOperationException();
+	}
+		
+	@Processor(
+			type = NodeAdapter.class,
+			priority = 1, 
+			value = "get() instanceof T(org.nasdanika.models.functionflow.SupplierFlow)")
+	public Object createSupplierFlowProcessor(
+			NodeProcessorConfig<?,?> config, 
+			boolean parallel, 
+			BiConsumer<Element,BiConsumer<ProcessorInfo<Object>,ProgressMonitor>> infoProvider,
+			Function<ProgressMonitor, Object> next,		
+			ProgressMonitor progressMonitor) {
+		
+		throw new UnsupportedOperationException();
+	}
+		
+	@Processor(
+			type = NodeAdapter.class,
+			value = "get() instanceof T(org.nasdanika.models.functionflow.Transition)")
+	public Object createTransitionProcessor(
+			ConnectionProcessorConfig<?,?> config, 
+			boolean parallel, 
+			BiConsumer<Element,BiConsumer<ProcessorInfo<Object>,ProgressMonitor>> infoProvider,
+			Function<ProgressMonitor, Object> next,		
+			ProgressMonitor progressMonitor) {
+		
+		throw new UnsupportedOperationException();
 	}
 
 }
